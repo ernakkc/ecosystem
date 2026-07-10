58179a2a444b4492a5b2ba05746bb10607da0b24
Eren Akkoç <ern.akkc@gmail.com>
2026-07-10 15:00:05 +0300

    General Files

---
+++ Scripts/World/World.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class World : Node3D
+{
+	public override void _Ready()
+	{
+		GD.Print("World Loaded");
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
